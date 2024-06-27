979bb0a3c523601cefee3059739dbe14f0b7436c
sscodez <ssameershah1200@gmail.com>
2024-06-27 19:25:16 +0500

    Added

--- src/components/common/gato-section.tsx
+++ src/components/common/gato-section.tsx
@@ -1,24 +1,33 @@
 import React from "react";
 import Image from "next/image";
 import Rowbreak from "./row-break";
+import useWidth from "../../utils/useWidth"; // Import the custom hook
+
 const GatoSection = () => {
+  const width = useWidth();
+
   return (
     <div className="mt-8 mb-5 w-[90%] md:w-[80%] text-center">
       <div className="flex justify-center items-center space-x-4 mb-4">
         <Image
           src="/assests/gato-ecosystem.png"
           alt="Logo 1"
-          width={300}
-          height={300}
+          width={width > 768 ? 300 : 150} // Adjust width based on screen size
+          height={width > 768 ? 300 : 150}
         />
         <Rowbreak />
-        <Image src="/assests/gusd.png" alt="Logo 2" width={150} height={150} />
+        <Image
+          src="/assests/gusd.png"
+          alt="Logo 2"
+          width={width > 768 ? 150 : 75} // Adjust width based on screen size
+          height={width > 768 ? 150 : 75}
+        />
         <Rowbreak />
         <Image
           src="/assests/gato-scan.png"
           alt="Logo 3"
-          width={200}
-          height={200}
+          width={width > 768 ? 200 : 100} // Adjust width based on screen size
+          height={width > 768 ? 200 : 100}
         />
       </div>
     </div>
